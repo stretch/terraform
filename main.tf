a03ee19eb5945f5c094c20fd58ed93ff090375f4
Indy Bains <ibains@capgemini.com>
2015-11-14 18:38:50 +0000

    Initial google config

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "google_compute_network" "stretch" {
+    name = "stretch"
+    ipv4_range = "10.0.1.0/24"
+}
+
+provider "google" {
+    account_file = "${file("google.json")}"
+    project = "winged-precinct-95712"
+    region = "europe-west1"
+}
